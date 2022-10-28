175aa7b31d52583223fa89a105b11e8c1c9b379f
MegRywalska <89724135+MegRywalska@users.noreply.github.com>
2022-10-28 21:35:32 +0200

    Initial commit

---
+++ src/main/java/com/matchmaker/matchmaker/MatchmakerApplication.java
@@ -0,0 +1,13 @@
+package com.matchmaker.matchmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchmakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchmakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matchmaker/matchmaker/MatchmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchmaker.matchmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

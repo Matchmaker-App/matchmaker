28e47f771606298aa83d34ea51b9728acbb88a69
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2022-10-31 18:36:49 +0100

    The user class model was added with the fields: id, username, password, email, first and last name. UserRepository interface added. Annotations have been added to both classes.

--- src/test/java/com/matchmaker/matchmaker/MatchmakerApplicationTests.java
+++ src/test/java/com/matchmaker/matchmaker/MatchmakerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.matchmaker.matchmaker;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
---
+++ src/test/java/com/matchmaker/matchmaker/user/User.java
@@ -0,0 +1,29 @@
+package com.matchmaker.matchmaker.user;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDay;
+
+
+}
